cc7da8ec6d9187cea797a5e7147b71cbe557a17c
Paulo Vargatt <pvargatt@gmail.com>
2017-12-18 01:41:18 -0200

    new2

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -51,6 +51,7 @@
 @if(config('adminlte.plugins.select2'))
     <!-- Select2 -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/i18n/pt-BR.js"></script>
 @endif
 
 @if(config('adminlte.plugins.datatables'))
